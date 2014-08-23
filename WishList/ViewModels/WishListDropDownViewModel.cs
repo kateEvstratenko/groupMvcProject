4d619c521770d34bf2725674bdcb8975940b1af6
kate <kate_evstratenko@mail.ru>
2014-08-24 02:21:08 +0300

    added views

---
+++ WishList/ViewModels/WishListDropDownViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WishList.ViewModels
+{
+    public class WishListDropDownViewModel
+    {
+       // public int Id { get; set; }
+        public string WishListId { get; set; }
+        public int GiftId { get; set; }
+
+        public SelectList DropDownList { get; set; }
+    }
+}
